93093f9e8d9840334eda08a8300779f94b3b8dfa
Justin Leemans <justintentje64@gmail.com>
2024-05-17 19:24:25 +0200

    Added initial console app

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ChatSample
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var program = new Program();
+        }
+        
+        private Program()
+        {
+        }
+    }
+}
